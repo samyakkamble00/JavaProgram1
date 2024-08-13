3deb0218b364a88016e7f44170bc75d2a5fea973
samyakkamble00 <samyakk07@gmail.com>
2024-08-13 13:57:28 +0530

    while

--- Java-Pratice/FirstCode.java
+++ Java-Pratice/FirstCode.java
@@ -1,22 +1,31 @@
 public class FirstCode{
     public static void main(String[] args) {
     //  System.out.println("Hello World");   \
-        int n = 0;
-        switch (n){
-            case 1:
-            System.out.println("1");
-            break;
-            case 2:
-            System.out.println("2");
-            break;
-            case 3:
-            System.out.println("3");
-            break;
-            case 4:
-            System.out.println("4");
-            default:
-            System.out.println("Default");
-            break;
-        }
+       
+    // switch
+        // switch (n){
+        //     case 1:
+        //     System.out.println("1");
+        //     break;
+        //     case 2:
+        //     System.out.println("2");
+        //     break;
+        //     case 3:
+        //     System.out.println("3");
+        //     break;
+        //     case 4:
+        //     System.out.println("4");
+        //     default:
+        //     System.out.println("Default");
+        //     break;
+        // }
+    // Loop
+        //while loop
+        // long i = 0;
+        // while (i <333) {
+        //     System.out.println(i);
+        //     i++;            
+        // }
+        
     }
 }
