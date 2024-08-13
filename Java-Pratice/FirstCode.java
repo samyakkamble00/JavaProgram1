07643fc422965e06905a8796995f437d31869ac0
samyakkamble00 <samyakk07@gmail.com>
2024-08-13 13:47:59 +0530

    switch

--- Java-Pratice/FirstCode.java
+++ Java-Pratice/FirstCode.java
@@ -1,5 +1,22 @@
-class FirstCode{
-public static void main(String[] args) {
- System.out.println("Hello World");   
-}
+public class FirstCode{
+    public static void main(String[] args) {
+    //  System.out.println("Hello World");   \
+        int n = 0;
+        switch (n){
+            case 1:
+            System.out.println("1");
+            break;
+            case 2:
+            System.out.println("2");
+            break;
+            case 3:
+            System.out.println("3");
+            break;
+            case 4:
+            System.out.println("4");
+            default:
+            System.out.println("Default");
+            break;
+        }
+    }
 }
