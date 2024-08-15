0160ab5d3f85193b991495887c3618c5524c9a74
samyakkamble00 <samyakk07@gmail.com>
2024-08-15 15:53:22 +0530

    array

---
+++ Array/Array1.java
@@ -0,0 +1,9 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int Arr[] ={1,2,3,4,5,6,7,8,9,0};
+
+        for(int n: Arr){
+            System.out.print(n + " ");
+        }
+    }
+}
