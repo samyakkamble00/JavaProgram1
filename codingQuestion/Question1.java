58605b1ce5c3643798fe01d3c7ebc0f1e581504b
samyakkamble00 <samyakk07@gmail.com>
2024-08-18 16:08:33 +0530

    question1 1 to 100

---
+++ codingQuestion/Question1.java
@@ -0,0 +1,10 @@
+public class Question1 {
+    //print number from 1 to 100 without using any number in your code
+    public static void main(String[] args) {
+        int one = 'A'/'A';
+        String str="..........";
+        for(int i =one;i<= str.length()*str.length();i++){
+            System.out.println(i);
+        }
+    }
+}
