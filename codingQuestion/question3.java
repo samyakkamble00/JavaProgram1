0d5f8202c39ad3273e4971accd540bd19bdcc95d
samyakkamble00 <samyakk07@gmail.com>
2024-08-18 16:42:20 +0530

    q4

---
+++ codingQuestion/question3.java
@@ -0,0 +1,9 @@
+// print hello world without using ; 
+public class question3 {
+    public static void main(String[] args) {
+        if(System.out.printf("Hello world") == null){
+
+        }
+        
+    }
+}
---
+++ codingQuestion/question4.java
@@ -0,0 +1,15 @@
+// NaN
+public class question4 {
+public static void main(String[] args){
+    Integer num1 = 100;
+    Integer num2 = 100;
+    System.out.println(num1);
+    System.out.println(num2);
+    if (num1 == num2) {
+        System.out.println("both are equals");
+    } else {
+        
+        System.out.println("both are not equals");
+    }
+}    
+}
